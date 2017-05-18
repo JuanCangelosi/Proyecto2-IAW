16f95239c7b5b913809bb527787cb293b244c4b6
Juan <juani.cangelosi@gmail.com>
2017-05-18 11:49:37 -0300

    modificaciones menores ventana admin

--- resources/views/cargaprecargados.blade.php
+++ resources/views/cargaprecargados.blade.php
@@ -7,10 +7,17 @@
 @section ('bodyContent')
 <div class="container-fluid panel-formulario" >
     <div class="row">
+           <div class="col-4 col-md-4 " id="table_acc">
+                <button type="submit" class="btn btn-primary">Agregar Característica</button>
+                <button type="submit" class="btn btn-primary">Modificar Característica</button>
+                <button type="submit" class="btn btn-primary">Modificar Precargados</button>
+                <button type="submit" class="btn btn-primary">Agregar Precargados</button>
+            </div>
+        <!-- lo de aca adentro debe ir en un js-->
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading headTextForm">Administrador</div>
-
+                
                 <!-- Menu de seleccion de objetos, cada h3 es un colapsible que uestra el div inmediatamente debajo -->
             <div class="col-3 col-md-12 "><div class="col-md-12" id="accordion" >
 
@@ -40,6 +47,9 @@
               <h3 data-toggle="collapse" href="#collapseLentes" aria-expanded="false" aria-controls="collapseLentes"><span class="caret-right" id="tab"></span>Agregar Vidrios</h3>
               <div class="collapse collapse-color container-fluid" id="collapseLentes">
                 <p id="mostrarLentes">
+                    <div class="checkbox">
+                        <label><input type="checkbox" value="">Tipo Nuevo?</label>
+                    </div>
                     <label for="example-text-input" class="col-2 col-form-label">Tipo</label>
                     <div class="col-10">
                             <input class="form-control" type="text" value="Original_Wayfarer" id="nombre_modelo">
@@ -57,6 +67,9 @@
               <h3 data-toggle="collapse" href="#collapseMarcos" aria-expanded="false" aria-controls="collapseMarcos"><span class="caret-right" id="tab"></span>Agregar Marcos</h3>
               <div class="collapse container-fluid" id="collapseMarcos">
                 <p id="mostrarMarcos">
+                    <div class="checkbox">
+                        <label><input type="checkbox" value="">Tipo Nuevo?</label>
+                    </div>
                     <label for="example-text-input" class="col-2 col-form-label">Tipo</label>
                     <div class="col-10">
                             <input class="form-control" type="text" value="Original_Wayfarer" id="nombre_modelo">
@@ -74,6 +87,9 @@
               <h3 data-toggle="collapse" href="#collapsePatillas" aria-expanded="false" aria-controls="collapsePatillas"><span class="caret-right" id="tab"></span>Agregar Patillas</h3>
               <div class="collapse container-fluid" id="collapsePatillas">
                  <p id="mostrarPatillas">
+                    <div class="checkbox">
+                        <label><input type="checkbox" value="">Tipo Nuevo?</label>
+                    </div>
                     <label for="example-text-input" class="col-2 col-form-label">Tipo</label>
                     <div class="col-10">
                             <input class="form-control" type="text" value="Original_Wayfarer" id="nombre_modelo">
@@ -108,7 +124,6 @@
                 </p>
               </div>
             </div></div>
-            </div>
             </div>
         </div>
     </div>
